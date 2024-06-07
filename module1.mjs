43600d3c05df31e0fce92b3890c650e05195bc79
Abdul Sattar <119532391+abdulsattar0617@users.noreply.github.com>
2024-06-07 20:52:05 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,10 @@
+
+import fruit, {game, sweet, sport} from './module2.mjs';
+
+console.log(`Fruits is ${fruit}`); 
+console.log(`Game is ${game}`); 
+console.log(`Sweet is ${sweet}`);
+console.log(`Sport is ${sport}`); 
+
+
+
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const fruit = "Apple";
+const game = "Tic Tac Toe";
+const sweet = "Double decker";
+const sport = "Racing";
+
+export default fruit; 
+export {game}; 
+export {sweet}; 
+export {sport}; 
